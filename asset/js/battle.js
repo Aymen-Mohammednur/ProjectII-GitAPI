87858cff7c613418b300ebb3fbf971532c704ec9
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-02-11 17:36:45 +0300

    Added userDetail function which includess the details to be displayed

--- asset/js/battle.js
+++ asset/js/battle.js
@@ -10,3 +10,38 @@ const battleButton = document
 const loaderDOM = document.querySelector(".loader");
 const cardDOM = document.querySelector("#cardDOM");
 const deckDOM = document.querySelector(".Cards__lists");
+
+function UserDetail(data) {
+    const {
+      login,
+      avatar_url,
+      html_url,
+      followers,
+      location,
+      name,
+      public_repos,
+      repos_url,
+    } = data;
+  
+    let forks = 0,
+      open_issues = 0,
+      stargazers_count = 0,
+      watchers = 0,
+      score = 0;
+  
+    return {
+      login,
+      avatar_url,
+      html_url,
+      followers,
+      location,
+      name,
+      public_repos,
+      repos_url,
+      forks,
+      open_issues,
+      stargazers_count,
+      watchers,
+      score,
+    };
+}
