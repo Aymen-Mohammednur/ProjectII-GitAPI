373d165150b599efdf9b09a0db417f01fa1d806e
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-02-11 16:43:18 +0300

    Variable declaration on battle section

---
+++ asset/js/battle.js
@@ -0,0 +1,12 @@
+const userDOM = document.querySelector(".Battlers__user");
+const usersListDOM = document.querySelector(".Battlers__users");
+const addUserButton = document
+  .querySelector(".Battlers__add")
+  .querySelector("button");
+const inputUserDOM = document.querySelector(".Battlers__input--input");
+const battleButton = document
+  .querySelector(".Battlers__submit")
+  .querySelector("button");
+const loaderDOM = document.querySelector(".loader");
+const cardDOM = document.querySelector("#cardDOM");
+const deckDOM = document.querySelector(".Cards__lists");
