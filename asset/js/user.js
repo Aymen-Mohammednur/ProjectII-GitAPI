f05c943f1ec731554c1a9e644c82c99e5af9049f
aben-bel <abenezer.sbelay@gmail.com>
2021-02-16 21:32:56 +0300

    fetch from github

--- asset/js/user.js
+++ asset/js/user.js
@@ -7,3 +7,27 @@ const searchBtnDOM = document
 const loaderDOM = document.querySelector(".loader");
 
 let username = "aben-bel";
+
+
+function getRepoWithLanguage(username = "aben-bel") {
+  return fetchUserRepoWithLang(username).then((data) => {
+    // return data;
+    //  console.log(data);
+    let repositories = data.repositoryOwner.repositories.edges;
+    let repos = repositories.map((ele) => {
+      const { totalSize } = ele.node.languages;
+      return {
+        repository: ele.node.name, // name
+        url: ele.node.url, // link to repository
+        languages: ele.node.languages.edges.map((lang) => {
+          return {
+            language: lang.node.name,
+            percent: (lang.size / totalSize) * 100,
+          };
+        }),
+      };
+    });
+    console.log(repos);
+    return repos;
+  });
+}
