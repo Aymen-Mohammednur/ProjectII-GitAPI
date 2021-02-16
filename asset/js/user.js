e47f5d9bb6f807915f5e492bda344aad7abbe40e
aben-bel <abenezer.sbelay@gmail.com>
2021-02-16 17:31:52 +0300

    Declare variable

---
+++ asset/js/user.js
@@ -0,0 +1,9 @@
+const languageDOM = document.querySelector(".Language__box");
+const deckDOM = document.querySelector(".Cards__lists");
+const inputDOM = document.querySelector(".Battlers__input--input");
+const searchBtnDOM = document
+  .querySelector(".Battlers__add")
+  .querySelector("button");
+const loaderDOM = document.querySelector(".loader");
+
+let username = "aben-bel";
